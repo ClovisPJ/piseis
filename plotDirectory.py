5d66aaeb4f243390530c2576e140299852a5263c
GeorgeOG <george.garber@me.com>
2014-10-10 13:04:00 +0100

    Update plotDirectory.py

--- plotDirectory.py
+++ plotDirectory.py
@@ -2,11 +2,9 @@ from obspy.core import read
 import os
 checkedfiles = []
 def plotDirectory (directory, previousfiles):
-    #totalstream = ''
     isfirst = 0
     isfirst += 1
     for f in os.listdir(directory):
-        
         alreadychecked = False
         for fil in previousfiles:
             if f == fil:
