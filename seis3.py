5882b843a4b08c7b2f3015b2fd3f6dd8d3274b24
ClovisPJ <cpj234@gmail.com>
2014-12-21 11:59:07 +0000

    seis3 formatting

--- seis3.py
+++ seis3.py
@@ -13,19 +13,17 @@ block_length=128
 #iterator for writing files
 block_id=0
 
-x=0
-
 #this is needed for saving in mseed so must be passed globably 
 global starttime
 global block_id
 
-#define the queue from using the library
+#define the queue from the function queue in the library queue
 q = Queue.Queue()
 
 
 def read_data(block_length):
 	starttime=UTCDateTime()
-	while x<block_length:
+	for x in range (block_length):
 		
 		#loop continues for block length
 	        sample = adc.readADCDifferential23(256, sps)*1000
@@ -33,7 +31,6 @@ def read_data(block_length):
 		#'timenow' not essential at the moment and isn't stored
 	       	timenow=UTCDateTime()
 	       	print sample,timenow
-		x=x+1
 		q.put(sample)
 
 
@@ -41,7 +38,7 @@ def read_data(block_length):
 #this is the worker thread
 def save_data():
 	while True:
-		if (q.qsize()>=block_length):
+		if q.qsize() >= block_length:
 			data=numpy.zeros([block_length],dtype=numpy.int16)
 
 			for x in range (block_length):	
@@ -53,16 +50,14 @@ def save_data():
 	        		 'channel': 'BHZ', 'npts': block_length, 'sampling_rate': 20,
 			         'mseed': {'dataquality': 'D'},'starttime': starttime}
 			
-			st =Stream([Trace(data=data, header=stats)])
-			to_save.write('mseed/PHYS' + str(block_id) + '.mseed',format='MSEED')
-			block_id=block_id+1
-		else:
-			print 'nothing to save...'
+			stream =Stream([Trace(data=data, header=stats)])
+			stream.write('mseed/MSEED' + str(block_id) + '.mseed',format='MSEED',encoding='INT16',reclen=512)
+			block_id += 1
 
 
 
-worker = Thread(target=save_data)
-worker.start()
+thread = Thread(target=save_data)
+thread.start()
 
 for x in range(5):
 	read_data(block_length)
